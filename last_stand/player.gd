7e8677c6fa56065b63ed896f1a8b09ea235af4e5
arko <arkadiusz.kupracz@gmail.com>
2026-02-15 21:22:57 +0000

    feat: player attack animation

--- last_stand/player.gd
+++ last_stand/player.gd
@@ -4,26 +4,39 @@ extends CharacterBody2D
 const PLAYER_SPEED = 300
 
 
+var is_attacking := false
+
 func _physics_process(_delta) -> void:
-	var movement_direction = Vector2.ZERO
-	if Input.is_action_pressed("move_down"):
-		movement_direction.y += 1
-	if Input.is_action_pressed("move_up"):
-		movement_direction.y -= 1
-	if Input.is_action_pressed("move_left"):
-		movement_direction.x -= 1
-	if Input.is_action_pressed("move_right"):
-		movement_direction.x += 1
-	
-	if movement_direction.x == 1:
-		$Sprite.flip_h = false
-	if movement_direction.x == -1:
-		$Sprite.flip_h = true
-	
-	if movement_direction == Vector2.ZERO:
-		$Sprite.animation = "idle"
-	else:
-		$Sprite.animation = "walk"
-		velocity = movement_direction.normalized() * PLAYER_SPEED
-		move_and_slide()
-		position = position.clamp(Vector2.ZERO + $Collision.shape.size * 0.5, get_viewport_rect().size - $Sprite.sprite_frames.get_frame_texture("walk", 0).get_size() * 0.5)
+	if not is_attacking:
+		if Input.is_action_just_pressed("attack"):
+			$Sprite.animation = "attack"
+			is_attacking = true
+		else:
+			var movement_direction = Vector2.ZERO
+			if Input.is_action_pressed("move_down"):
+				movement_direction.y += 1
+			if Input.is_action_pressed("move_up"):
+				movement_direction.y -= 1
+			if Input.is_action_pressed("move_left"):
+				movement_direction.x -= 1
+			if Input.is_action_pressed("move_right"):
+				movement_direction.x += 1
+			
+			if movement_direction.x == 1:
+				$Sprite.flip_h = false
+			if movement_direction.x == -1:
+				$Sprite.flip_h = true
+			
+			velocity = movement_direction.normalized() * PLAYER_SPEED
+			if movement_direction == Vector2.ZERO:
+				$Sprite.animation = "idle"
+			else:
+				$Sprite.animation = "walk"
+			$Sprite.play()
+			
+			move_and_slide()
+			position = position.clamp(Vector2.ZERO + $Collision.shape.size * 0.5, get_viewport_rect().size - $Sprite.sprite_frames.get_frame_texture("walk", 0).get_size() * 0.5)
+
+
+func _on_sprite_animation_finished() -> void:
+	is_attacking = false
--- last_stand/zombie.gd
+++ last_stand/zombie.gd
@@ -5,9 +5,10 @@ const ZOMBIE_SPEED := 100
 
 func _ready() -> void:
 	$Sprite.flip_h = true
+	$Sprite.play("walk")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity = Vector2(-1, 0).normalized() * ZOMBIE_SPEED
 	if move_and_slide():
 		$Sprite.animation = "attack"
