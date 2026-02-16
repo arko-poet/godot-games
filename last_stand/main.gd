7bfd098eece0a52efce37fbbe4e802cf537fe674
arko <arkadiusz.kupracz@gmail.com>
2026-02-16 20:25:05 +0000

    feat: resizable cursor

--- last_stand/main.gd
+++ last_stand/main.gd
@@ -6,12 +6,9 @@ const ZOMBIE_SCENE := preload("res://zombie.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.set_custom_mouse_cursor(load("res://sprites/crosshair/crosshair.png"))
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+	var crosshair : Image = load("res://sprites/crosshair/crosshair.png").get_image()
+	crosshair.resize(24, 24)
+	Input.set_custom_mouse_cursor(crosshair)
 
 
 func _on_zombie_spawner_timeout() -> void:
