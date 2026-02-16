ba11a8320ad282d4ff5efdda8024b7d02ce69571
arko <arkadiusz.kupracz@gmail.com>
2026-02-16 14:05:12 +0000

    feat: kill zombies on projectile collision

--- last_stand/projectile.gd
+++ last_stand/projectile.gd
@@ -10,13 +10,13 @@ var direction : Vector2
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	position += direction * SPEED * delta
 
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	direction = Vector2.ZERO
 	$Sprite.animation = "explode"
 	$Sprite.animation_finished.connect(queue_free)
+	body.hit()
--- last_stand/zombie.gd
+++ last_stand/zombie.gd
@@ -3,12 +3,20 @@ extends CharacterBody2D
 
 const ZOMBIE_SPEED := 100
 
+var alive := true
+
 func _ready() -> void:
 	$Sprite.flip_h = true
 	$Sprite.play("walk")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	velocity = Vector2(-1, 0).normalized() * ZOMBIE_SPEED
-	if move_and_slide():
-		$Sprite.animation = "attack"
+	if alive:
+		velocity = Vector2(-1, 0).normalized() * ZOMBIE_SPEED
+		if move_and_slide():
+			$Sprite.animation = "attack"
+
+func hit() -> void:
+	$Sprite.animation = "die"
+	alive = false
+	$Collision.queue_free()
