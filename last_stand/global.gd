9a2ceb2caa14ba3aa56c4b9cddaf65c7c9a8c21a
arko <arkadiusz.kupracz@gmail.com>
2026-02-19 16:25:45 +0000

    feat: upgrade weapon

--- last_stand/global.gd
+++ last_stand/global.gd
@@ -3,9 +3,11 @@ extends Node
 
 const MAX_ZOMBIES := 2
 const LEVEL_RESOURCES := 10
+const UPGRADE_WEAPON_COST := 6
 
 var level := 1
 var zombies_killed := 0
 var health := 100
 var resources := LEVEL_RESOURCES
-var number_of_projectiles := 3
+var number_of_projectiles := 1
+var upgrade_weapon_progress := 0
--- last_stand/upgrade_screen.gd
+++ last_stand/upgrade_screen.gd
@@ -7,6 +7,7 @@ signal next_level
 func _process(_delta) -> void:
 	$BarricadeHealth.text = "Barricade: %s" % Global.health
 	$Resources.text = str(Global.resources)
+	$VBoxContainer2/ProgressBar.value = Global.upgrade_weapon_progress
 
 
 func _on_next_level_pressed() -> void:
@@ -17,3 +18,14 @@ func _on_upgrade_barricade_pressed() -> void:
 	if Global.resources > 0:
 		Global.resources -= 1
 		Global.health += 10
+
+
+func _on_upgrade_weapon_pressed() -> void:
+	if Global.resources > 0:
+		Global.resources -= 1
+		if Global.upgrade_weapon_progress < 5:
+			Global.upgrade_weapon_progress += 1
+		else:
+			Global.upgrade_weapon_progress = 0
+			Global.number_of_projectiles += 1
+		
