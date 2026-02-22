8eea8a7e45eb03a9586de3f95eab76c560e6a3ce
arko <arkadiusz.kupracz@gmail.com>
2026-02-22 13:33:12 +0000

    feat: new game 'tower'

--- last_stand/barricade.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-
-
-func hit(damage : int):
-	Global.health -= damage
-	if Global.health <= 0:
-		Global.health = 0
-		queue_free()
--- last_stand/game_over.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-signal start_new_game
-
-
-func _on_try_again_button_pressed() -> void:
-	start_new_game.emit()
