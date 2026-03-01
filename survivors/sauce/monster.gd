3042319fffb476631c06fa7cb26f1b7784d1cb02
arko <arkadiusz.kupracz@gmail.com>
2026-03-01 14:03:03 +0000

    feat(survivors): add NavigationRegion2D to world

--- survivors/sauce/monster.gd
+++ survivors/sauce/monster.gd
@@ -5,12 +5,21 @@ const SPEED := 100.0
 
 var target : CharacterBody2D
 
+@onready var navigation: NavigationAgent2D = $Navigation
 
 func _physics_process(_delta: float) -> void:
 	if target:
-		var direction := target.global_position - global_position
-		velocity = direction.normalized() * SPEED
+		velocity = global_position.direction_to(navigation.get_next_path_position()) * SPEED
 		move_and_slide()
+		
+		
+		#var direction := target.global_position - global_position
+		#velocity = direction.normalized() * SPEED
+		#move_and_slide()
 
 func set_target(new_target : CharacterBody2D) -> void:
 	self.target = new_target
+
+
+func _on_update_target_position_timeout() -> void:
+	navigation.target_position = target.global_position
