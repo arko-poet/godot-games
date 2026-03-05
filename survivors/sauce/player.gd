ffee5b2746bd990146b0ed41986e94f3af1d4f4e
arko <arkadiusz.kupracz@gmail.com>
2026-03-05 14:53:46 +0000

    refactor(survivors): player manages weapons

--- survivors/sauce/player.gd
+++ survivors/sauce/player.gd
@@ -5,6 +5,14 @@ signal died
 signal hp_changed
 signal xp_changed
 signal level_changed
+enum WeaponID {BALLS, SWORD, CHAKRAMS, SPEARS}
+const WEAPON_SCENES := {
+	WeaponID.BALLS : preload("res://sauce/balls.tscn"),
+	WeaponID.SWORD : preload("res://sauce/sword.tscn"),
+	WeaponID.CHAKRAMS : preload("res://sauce/chakrams.tscn"),
+	WeaponID.SPEARS: preload("res://sauce/spears.tscn"),
+}
+var weapons : Array[Weapon] = []
 var speed := 150.0
 var xp := 0
 var next_level_xp := 10
@@ -13,6 +21,20 @@ var hp := 100
 var monsters_in_contact : Array[Monster] = []
 var level := 1
 @onready var sprite: AnimatedSprite2D = $Sprite
+
+
+func _ready() -> void:
+	add_weapon(WeaponID.BALLS)
+
+func add_weapon(weapon_id : WeaponID) -> void:
+	var weapon_scene : PackedScene = WEAPON_SCENES[weapon_id]
+	if weapon_scene != null:
+		var weapon : Weapon = weapon_scene.instantiate()
+		weapon.projectile_root = get_parent()
+		add_child(weapon)
+		weapons.append(weapon)
+	else:
+		push_error("invalid weapon_id: %s" % weapon_id)
 
 
 func _physics_process(_delta: float) -> void:
