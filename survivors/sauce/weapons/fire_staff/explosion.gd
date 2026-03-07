ed80ae3b848f77730c2bb0625c334fe95b001041
arko <arkadiusz.kupracz@gmail.com>
2026-03-07 12:58:02 +0000

    feat(survivors): add fireball and explosion fx

--- survivors/sauce/weapons/fire_staff/explosion.gd
+++ survivors/sauce/weapons/fire_staff/explosion.gd
@@ -4,11 +4,17 @@ extends Area2D
 var knockback_velocity := 200.0
 var damage : int
 
+@onready var particles: CPUParticles2D = $Particles
+
 
 func _ready():
 	await get_tree().physics_frame
+	particles.emitting = true
 	var bodies := get_overlapping_bodies()
 	for body in bodies:
 		if body.is_in_group("monsters"):
 			body.hit(1, (body.global_position - global_position).normalized() * knockback_velocity)
+
+
+func _on_particles_finished() -> void:
 	queue_free()
