298f2641e3d42d1d7049cde01a211eaa1c72dd1f
arko <arkadiusz.kupracz@gmail.com>
2026-03-10 13:33:47 +0000

    feat(survivors): animate arrows in upgrade menu

--- survivors/sauce/hud/upgrade_menu.gd
+++ survivors/sauce/hud/upgrade_menu.gd
@@ -3,12 +3,15 @@ extends Control
 signal upgrade_chosen(upgrade: Upgrade)
 
 var selected_card := 1
+var arrow_bob_switch := false ## arrow bob left or right
+
 @onready var cards := {
 	1: $MainPanel/UpgradeCards/Card1,
 	2: $MainPanel/UpgradeCards/Card2,
 	3: $MainPanel/UpgradeCards/Card3
 }
-@onready var arrow: TextureRect = $Arrow
+@onready var left_arrow: TextureRect = $LeftArrow
+@onready var right_arrow: TextureRect = $RightArrow
 
 
 func _ready() -> void:
@@ -27,8 +30,9 @@ func _set_arrow_position() -> void:
 	if not card:
 		push_error("invalid slected_card index: %s" % selected_card)
 		return
-	arrow.global_position.y = card.global_position.y + 0.5 * (card.size.y - arrow.size.y)
-
+	var y = card.global_position.y + 0.5 * (card.size.y - left_arrow.size.y)
+	left_arrow.global_position.y = y
+	right_arrow.global_position.y = y
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not is_visible_in_tree():
@@ -41,3 +45,13 @@ func _unhandled_input(event: InputEvent) -> void:
 		_set_arrow_position()
 	elif event.is_action_pressed("ui_accept"):
 		emit_signal("upgrade_chosen", cards[selected_card].upgrade)
+
+
+func _on_arrow_animation_timer_timeout() -> void:
+	if arrow_bob_switch:
+		left_arrow.global_position.x += 2
+		right_arrow.global_position.x -= 2
+	else:
+		left_arrow.global_position.x -= 2
+		right_arrow.global_position.x += 2
+	arrow_bob_switch = not arrow_bob_switch
