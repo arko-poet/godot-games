16eda169c04ab4dcd5521e431b2658e8bce9d39d
arko <arkadiusz.kupracz@gmail.com>
2026-03-10 10:41:27 +0000

    feat(survivors): add rarity system

--- survivors/sauce/hud/upgrade_card.gd
+++ survivors/sauce/hud/upgrade_card.gd
@@ -1,7 +1,22 @@
 extends PanelContainer
 
+const RARITY_COLORS := {
+	UpgradeService.Rarity.COMMON: Color("C8CDD8"),
+	UpgradeService.Rarity.RARE: Color("3A8DFF"),
+	UpgradeService.Rarity.EPIC: Color("A335EE"),
+	UpgradeService.Rarity.LEGENDARY: Color("FF9F1C")
+}
+
+const RARITY_NAMES := {
+	UpgradeService.Rarity.COMMON: "COMMON",
+	UpgradeService.Rarity.RARE: "RARE",
+	UpgradeService.Rarity.EPIC: "EPIC",
+	UpgradeService.Rarity.LEGENDARY: "LEGENDARY"
+}
+
 @onready var name_label: Label = $TextContainer/HBoxContainer/Name
 @onready var description: Label = $TextContainer/Description
+@onready var rarity_label: Label = $TextContainer/HBoxContainer/Rarity
 
 var upgrade: Upgrade
 
@@ -14,3 +29,11 @@ func set_upgrade(new_upgrade: Upgrade):
 		description.text = "Increase %s by %s" % [upgrade.property, upgrade.value]
 	else:
 		description.text = "NEW WEAPON"
+	
+	var rarity_color : Color = RARITY_COLORS[upgrade.rarity]
+	rarity_label.add_theme_color_override("font_color", rarity_color)
+	rarity_label.text = "[%s]" % RARITY_NAMES[upgrade.rarity]
+	
+	var stylebox := get_theme_stylebox("panel").duplicate()
+	stylebox.border_color = rarity_color
+	add_theme_stylebox_override("panel", stylebox)
--- survivors/sauce/upgrades/upgrade.gd
+++ survivors/sauce/upgrades/upgrade.gd
@@ -1,11 +1,10 @@
 class_name Upgrade
 extends RefCounted
 
-enum Rarity {COMMON, RARE, EPIC, LEGENDARY} 
 
 var name_: String
 var property: String
 var value: float
 var icon_path: String
-var rarity: Rarity
+var rarity: int
 var id: int ## weapon type id
