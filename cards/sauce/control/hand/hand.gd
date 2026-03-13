5131b90ab02c9aab2a377d78eb341d9d750747d4
arko <arkadiusz.kupracz@gmail.com>
2026-03-13 16:49:25 +0000

    fix(cards): Hand Container positioning

--- cards/sauce/control/hand/hand.gd
+++ cards/sauce/control/hand/hand.gd
@@ -2,10 +2,9 @@
 class_name Hand
 extends Container
 
-var cards : Array[PanelContainer] = []
+var cards : Array[Control] = []
 
 @export var max_angle: float = 18.0 ## maximum card spread
-@export var radius: float = 1200.0 ## fan radius
 @export var card_size := Vector2(160, 225)
 @export var angle_per_card := 3.0 ## ideal spread per card
 
@@ -14,19 +13,27 @@ func _ready():
 	if Engine.is_editor_hint():
 		queue_sort()
 
+func _process(_delta):
+	if Engine.is_editor_hint():
+		queue_sort()
+
 
 func _arrange_cards() -> void:
+	var fan_radius := size.x
+
 	var count = get_child_count()
 	var total_angle = min(max_angle, angle_per_card * (count - 1))
+	
 	for i in count:
-		var card: PanelContainer = get_child(i)
+		var card: Control = get_child(i)
 		var interpolation_weight = 0.5
 		if count > 1:
 			interpolation_weight = float(i) / (count - 1)
 			
 		var angle = deg_to_rad(lerp(-total_angle, total_angle, interpolation_weight))
 			
-		fit_child_in_rect(card, Rect2(Vector2(sin(angle), -cos(angle)) * radius, card_size))
+		var offset = Vector2(0.5 * (fan_radius - card_size.x), fan_radius)
+		fit_child_in_rect(card, Rect2(Vector2(sin(angle), -cos(angle)) * fan_radius + offset,  card_size))
 		card.rotation =  angle
 
 
@@ -47,3 +54,7 @@ func pop_card() -> void:
 	if not cards.is_empty():
 		var card = cards.pop_at(0)
 		card.queue_free()
+
+
+func _get_minimum_size() -> Vector2:
+	return card_size
