8bd390f649689c352c14e0d70f995aecf4b22ed9
arko <arkadiusz.kupracz@gmail.com>
2026-04-09 12:50:09 +0100

    feat(auto_battler): add item drag and drop

--- auto_battler/sauce/inventory/inventory.gd
+++ auto_battler/sauce/inventory/inventory.gd
@@ -3,7 +3,11 @@ extends Control
 const INVENTORY_SIZE := 8 ## height and width in number of cells
 const CELL_SIZE := Vector2i(16, 16)
 const BORDER_COLOR := Color("#5A6270")
-const CELL_BG := Color("#252A33")
+const BG_COLOR := Color("#252A33")
+
+
+func _ready() -> void:
+	custom_minimum_size = Vector2i(CELL_SIZE * INVENTORY_SIZE)
 
 
 func _draw():
@@ -11,5 +15,19 @@ func _draw():
 	for y in range(INVENTORY_SIZE):
 		for x in range(INVENTORY_SIZE):
 			var cell_position := Vector2i(x * CELL_SIZE.x, y * CELL_SIZE.y)
-			draw_rect(Rect2i(cell_position, CELL_SIZE), CELL_BG)
-			draw_rect(Rect2i(cell_position, CELL_SIZE), BORDER_COLOR, false, 1)
+			var rect2i := Rect2i(cell_position, CELL_SIZE)
+			draw_rect(rect2i, BG_COLOR)
+			draw_rect(rect2i, BORDER_COLOR, false, 1)
+
+
+func _can_drop_data(_at_position: Vector2, _data: Variant) -> bool:
+	return true
+
+
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+	assert(data is Dictionary)
+	assert("item" in data)
+	
+	var offset: Vector2 = data["offset"]
+	var item: Item = data["item"]
+	item.position = at_position + position + offset
---
+++ auto_battler/sauce/item/item.gd
@@ -0,0 +1,36 @@
+class_name Item
+extends Control
+
+@onready var sprite: ColorRect = $Sprite
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_DRAG_END:
+		show()
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index != MOUSE_BUTTON_LEFT:
+			return
+		assert(event.pressed)
+		
+		_start_dragging()
+
+
+func _start_dragging() -> void:
+	var sprite_preview := sprite.duplicate()
+	var offset = global_position - get_global_mouse_position()
+	sprite_preview.position = offset
+	
+	var preview := Control.new()
+	preview.size = size
+	preview.add_child(sprite_preview)
+	preview.mouse_default_cursor_shape = Control.CURSOR_DRAG
+	
+	var drag_data := {}
+	drag_data["offset"] = offset
+	drag_data["item"] = self
+	
+	force_drag.call_deferred(drag_data, preview)
+	hide()
