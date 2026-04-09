2f0c37dedc1e84347af0ff4eae8b447cdaf96b8c
arko <arkadiusz.kupracz@gmail.com>
2026-04-09 13:47:57 +0100

    assets(auto_battle): add custom cursors

---
+++ auto_battler/sauce/main/main.gd
@@ -0,0 +1,15 @@
+extends Node
+
+const DRAG_CURSOR := preload(
+	"res://assets/kenney_cursor-pack/PNG/Outline/Default/hand_closed.png"
+)
+const HOVER_CURSOR := preload(
+	"res://assets/kenney_cursor-pack/PNG/Outline/Default/gauntlet_point.png"
+)
+
+
+func _ready() -> void:
+	Input.set_custom_mouse_cursor(DRAG_CURSOR, Input.CURSOR_DRAG)
+	Input.set_custom_mouse_cursor(DRAG_CURSOR, Input.CURSOR_CAN_DROP)
+	Input.set_custom_mouse_cursor(DRAG_CURSOR, Input.CURSOR_FORBIDDEN)
+	Input.set_custom_mouse_cursor(HOVER_CURSOR, Input.CURSOR_POINTING_HAND)
