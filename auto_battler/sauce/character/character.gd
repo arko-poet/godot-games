cc697d83c0a4e9a94c32e14c08240de3a442c879
arko <arkadiusz.kupracz@gmail.com>
2026-04-10 10:55:09 +0100

    feat(auto_battler): add generic character scene

---
+++ auto_battler/sauce/character/character.gd
@@ -0,0 +1,22 @@
+extends Control
+
+signal died
+
+@export_range(1, 1000) var max_hp: int
+
+var hp: int:
+	set(val):
+		hp = max(val, 0)
+		hp_bar.value = hp
+		hp_bar.max_value = max_hp
+		hp_label.text = "%s/%s" % [hp, max_hp]
+		if hp == 0:
+			died.emit()
+		
+@onready var hp_bar: ProgressBar = $HPBar
+@onready var hp_label: Label = $HPBar/HPLabel
+
+
+func _ready() -> void:
+	assert(max_hp > 0)
+	hp = max_hp
