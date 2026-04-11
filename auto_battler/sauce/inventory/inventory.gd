df2e1265350b9748ae210db59b42d08ea6418852
arko <arkadiusz.kupracz@gmail.com>
2026-04-11 12:27:11 +0100

    feat(auto_battler): no multiple items in 1 cell

--- auto_battler/sauce/inventory/inventory.gd
+++ auto_battler/sauce/inventory/inventory.gd
@@ -9,11 +9,13 @@ const INVENTORY_SIZE := 8 ## height and width in number of cells
 const CELL_SIZE := Vector2i(16, 16)
 const BORDER_COLOR := Color("#5A6270")
 const BG_COLOR := Color("#252A33")
-const HOVER_COLOR := Color(0.0, 0.608, 0.0, 1.0)
+const CAN_DROP_BG_COLOR := Color(0.0, 0.608, 0.0, 1.0)
+const CANT_DROP_BG_COLOR := Color(0.69, 0.0, 0.0, 1.0)
 
 var item_grid: Array[Array] = [] ## Item if cell occupied, null otherwise
 var hovered_cells: Array[Vector2i] = [] ## currently hovered cells, used for redrawing grid
 var items: Array[Item] = []
+var hover_color: Color ## changes depending on if drop is allowed or not
 
 func _ready() -> void:
 	custom_minimum_size = Vector2i(CELL_SIZE * INVENTORY_SIZE)
@@ -37,26 +39,28 @@ func _draw():
 		for x in range(INVENTORY_SIZE):
 			var cell := Vector2i(x, y)
 			var rect2i := Rect2i(cell * CELL_SIZE, CELL_SIZE)
-			draw_rect(rect2i, HOVER_COLOR if cell in hovered_cells else BG_COLOR)
+			draw_rect(rect2i, hover_color if cell in hovered_cells else BG_COLOR)
 			draw_rect(rect2i, BORDER_COLOR, false, 1)
 
 
 func _can_drop_data(at_position: Vector2, _data: Variant) -> bool:
 	var hovered_cell := _position_to_cell(at_position)
+	var can_hover = item_grid[hovered_cell.y][hovered_cell.x] == null
+	hover_color = CAN_DROP_BG_COLOR if can_hover else CANT_DROP_BG_COLOR
 	if hovered_cell not in hovered_cells:
 		hovered_cells.clear()
 		hovered_cells.append(hovered_cell)
 		queue_redraw()
-	return true
+	return can_hover
 
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	var item: Item = data["item"]
-	var cell_index := _position_to_cell(at_position)
+	var cell := _position_to_cell(at_position)
 	if item in items:
-		_move_item(item, cell_index)
+		_move_item(item, cell)
 	else:
-		_add_item(item, cell_index)
+		_add_item(item, cell)
 	hovered_cells.clear()
 	queue_redraw()
 
@@ -93,15 +97,15 @@ func _position_to_cell(at_position: Vector2) -> Vector2i:
 	return cell
 
 
-func _add_item(item: Item, cell_index: Vector2i) -> void:
-	_place_item(item, cell_index)
+func _add_item(item: Item, cell: Vector2i) -> void:
+	_place_item(item, cell)
 	items.append(item)
 	item_added.emit(item)
 
 
-func _place_item(item: Item, cell_index: Vector2i) -> void:
-	item_grid[cell_index.y][cell_index.x] = item
-	item.position = Vector2i(position) + CELL_SIZE * cell_index
+func _place_item(item: Item, cell: Vector2i) -> void:
+	item_grid[cell.y][cell.x] = item
+	item.position = Vector2i(position) + CELL_SIZE * cell
 
 
 func _remove_item(item: Item) -> void:
@@ -113,9 +117,9 @@ func _remove_item(item: Item) -> void:
 	item_removed.emit(item)
 
 
-func _move_item(item: Item, cell_index: Vector2i) -> void:
+func _move_item(item: Item, cell: Vector2i) -> void:
 	for row in item_grid:
 		for col_i in range(len(row)):
 			if item == row[col_i]:
 				row[col_i] = null
-	_place_item(item, cell_index)
+	_place_item(item, cell)
