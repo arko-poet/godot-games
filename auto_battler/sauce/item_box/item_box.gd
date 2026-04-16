f660804a997d868c53c8b9393e74201a2e9f41ca
arko <arkadiusz.kupracz@gmail.com>
2026-04-16 14:30:38 +0100

    fix(ab): items in box are positioned correctly

--- auto_battler/sauce/item_box/item_box.gd
+++ auto_battler/sauce/item_box/item_box.gd
@@ -7,8 +7,12 @@ func _can_drop_data(_at_position: Vector2, _data: Variant) -> bool:
 	return true
 
 
-func _drop_data(_at_position: Vector2, data: Variant) -> void:
+func _drop_data(at_position: Vector2, data: Variant) -> void:
 	var item: Item = data["item"]
 	inventory.remove_item(item)
 	item.reparent(self)
-	item.position = _at_position - data["offset"]
+	
+	var offset: Vector2 = data["offset"]
+	for i in range(item.footprint_index):
+		offset = Vector2(-offset.y, offset.x)
+	item.position = at_position - offset
