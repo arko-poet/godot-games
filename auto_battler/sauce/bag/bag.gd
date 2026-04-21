b654fe682a4e38849263677028a3977bded7a35d
arko <arkadiusz.kupracz@gmail.com>
2026-04-21 18:56:34 +0100

    feat(ab): add base Bag Node

---
+++ auto_battler/sauce/bag/bag.gd
@@ -0,0 +1,27 @@
+@tool
+class_name Bag extends Control
+
+@export_range(1, Inventory.INVENTORY_SIZE) var columns: int = 1
+@export_range(1, Inventory.INVENTORY_SIZE) var rows: int = 1
+@export var bg_color: Color = Color("#6B3F1E")
+@export var border_color: Color = Color("#C4955A")
+
+var footprint: Array[Vector2i]
+
+
+func _ready() -> void:
+	size = Vector2(columns, rows) * Globals.CELL_SIZE
+	for row in rows:
+		for column in columns:
+			footprint.append(Vector2i(column, row))
+
+
+func _draw() -> void:
+	for row in rows:
+		for column in columns:
+			var cell := Vector2i(column, row)
+			var cell_position := cell * Globals.CELL_SIZE
+			var cell_size := Vector2i(Globals.CELL_SIZE, Globals.CELL_SIZE)
+			var rect2i := Rect2i(cell_position, cell_size)
+			draw_rect(rect2i, bg_color)
+			draw_rect(rect2i, border_color, false, 1)
