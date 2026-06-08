efdc50bec96736159d3a9d29f8ae8f4ede7736b1
arko <arkadiusz.kupracz@gmail.com>
2026-06-08 15:48:09 +0100

    chore(ab): clean code in rigid_component.gd

--- projects/auto_battler/sauce/rigid_components/rigid_component.gd
+++ projects/auto_battler/sauce/rigid_components/rigid_component.gd
@@ -2,12 +2,12 @@ class_name RigidComponent extends RigidBody2D
 
 const MAX_VELOCITIES_STORED := 10
 
-var dragging = false
+var is_dragging = false
 var drag_offset = Vector2.ZERO
 var inventory_component: InventoryComponent
 
 var recent_mouse_positions: Array[Vector2]
-var recent_time_elapsed: Array[float]
+var recent_deltas: Array[float]
 
 @onready var collision: CollisionShape2D = $Collision
 
@@ -21,15 +21,15 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 
 
 func _process(delta) -> void:
-	if dragging:
+	if is_dragging:
 		global_position = get_global_mouse_position() + drag_offset
 		
 		#for release impulse calculation
 		if recent_mouse_positions.size() == MAX_VELOCITIES_STORED:
 			recent_mouse_positions.pop_front()
-			recent_time_elapsed.pop_front()
+			recent_deltas.pop_front()
 		recent_mouse_positions.append(get_global_mouse_position())
-		recent_time_elapsed.append(delta)
+		recent_deltas.append(delta)
 
 
 #func _input(event: InputEvent) -> void:
@@ -45,32 +45,28 @@ func start_drag() -> void:
 
 	inventory_component.test_drag(global_position)
 
-	
 	drag_offset = global_position - get_global_mouse_position()
-	
-
 
 
 func stop_drag(p_position: Vector2) -> void:
 	switch()
 	global_position = p_position
 	
-	var change_in_position := recent_mouse_positions[recent_mouse_positions.size() - 1] - recent_mouse_positions[0]
-	var total_time := 0.0
-	for t in recent_time_elapsed:
-		total_time += t
-	var mouse_velocity = change_in_position / total_time
+	var change_in_position := recent_mouse_positions[-1] - recent_mouse_positions[0]
+	var total_delta := 0.0
+	for dt in recent_deltas:
+		total_delta += dt
+	var mouse_velocity = change_in_position / total_delta
 	
 	var mouse_direction := mouse_velocity.normalized()
 	var mouse_magnitude := mouse_velocity.length()
 	apply_central_impulse(mouse_direction * min(mouse_magnitude, get_gravity().length()))
 
 
-
-
+## enables/disable items interaction with the game
 func switch(on: bool = true) -> void:
 	freeze = !on
-	dragging = !on
+	is_dragging = !on
 	
 	for child in get_children():
 		if child is CollisionShape2D:
