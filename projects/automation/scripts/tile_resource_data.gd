b838aa01a9a7724b272d784af8b79a748520b78b
arko <arkadiusz.kupracz@gmail.com>
2026-06-28 15:28:38 +0100

    feat(automation): detect resource hover input

--- projects/automation/scripts/tile_resource_data.gd
+++ projects/automation/scripts/tile_resource_data.gd
@@ -1,14 +1,18 @@
 class_name TileResourceData extends RefCounted
 
-
 enum ResourceType {
 	COAL
 }
 
 var resource: ResourceType
 var quantity: int
+var name: String
 
 
 func _init(p_resource: ResourceType, p_quantity: int = 100) -> void:
 	resource = p_resource
 	quantity = p_quantity
+
+
+func get_name() -> String:
+	return ResourceType.keys()[resource]
