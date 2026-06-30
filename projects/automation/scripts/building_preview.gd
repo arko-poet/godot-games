6e8eca1502bca4d94910f66c6bfc467074614d4b
arko <arkadiusz.kupracz@gmail.com>
2026-06-30 16:54:10 +0100

    feat(automation): snap building preview to grid

--- projects/automation/scripts/building_preview.gd
+++ projects/automation/scripts/building_preview.gd
@@ -1,5 +1,23 @@
 extends Node2D
 
+var footprint: Array[Vector2i]
+var center_tile: Vector2i
+
+var tile_map_layer: TileMapLayer
+
+
+func _ready() -> void:
+	for i in 3:
+		for j in 3:
+			footprint.append(Vector2i(i, j))
+	
+	center_tile = Vector2i(1, 1)
+
 
 func _process(_delta: float) -> void:
-	position = get_global_mouse_position()
+	if not tile_map_layer:
+		return
+	
+	var mouse_tile := tile_map_layer.local_to_map(get_global_mouse_position())
+	position = tile_map_layer.map_to_local(mouse_tile)
+	
--- projects/automation/scripts/world.gd
+++ projects/automation/scripts/world.gd
@@ -5,7 +5,10 @@ const TILE_SIZE := 32
 
 const BuildingPreview := preload("res://scenes/building_preview.tscn")
 
+@onready var building_layer: TileMapLayer = $TileMapLayers/BuildingLayer
+
 
 func _on_create_mine_pressed() -> void:
 	var building_preview := BuildingPreview.instantiate()
+	building_preview.tile_map_layer = building_layer
 	add_child(building_preview)
