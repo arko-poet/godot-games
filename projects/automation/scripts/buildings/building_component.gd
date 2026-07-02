9944d320e7a578ebd70b694ea44ba79529e7f3a1
arko <arkadiusz.kupracz@gmail.com>
2026-07-02 15:51:12 +0100

    feat(automation): add building component node

---
+++ projects/automation/scripts/buildings/building_component.gd
@@ -0,0 +1,18 @@
+class_name BuildingComponent extends Node2D
+
+signal timeout
+
+@export var texture: Texture2D
+@export var production_time: float = 1.0
+
+@onready var _sprite: Sprite2D = $Sprite
+@onready var _production_timer: Timer = $ProductionTimer
+
+
+func _ready() -> void:
+	_sprite.texture = texture
+	_production_timer.wait_time = production_time
+
+
+func _on_production_timer_timeout() -> void:
+	timeout.emit()
