3778f3b05a31b49e3138acc1b23d9609d79c513c
arko <arkadiusz.kupracz@gmail.com>
2026-07-25 20:17:21 +0100

    feat(automation): move assembler code to furnance

--- projects/automation/scripts/buildings/assembler.gd
+++ projects/automation/scripts/buildings/assembler.gd
@@ -8,30 +8,3 @@ extends Node2D
 
 func _ready():
 	production_timer.start()
-
-
-func _on_production_timer_timeout() -> void:
-	if not storage.has_stored_resource(Resources.Type.COAL):
-		return
-	if storage.has_stored_resource(Resources.Type.COPPER):
-		_smelt_copper()
-	elif storage.has_stored_resource(Resources.Type.SILVER):
-		_smelt_silver()
-
-
-func _smelt_copper() -> void:
-	if not storage.withdraw_stored_resource(Resources.Type.COAL):
-		push_error("No Coal in storage")
-	if not storage.withdraw_stored_resource(Resources.Type.COPPER):
-		push_error("No copper in storage")
-	
-	storage.store_resources(Resources.Type.COPPER_BAR, 1)
-	
-
-func _smelt_silver() -> void:
-	if not storage.withdraw_stored_resource(Resources.Type.COAL):
-		push_error("No Coal in storage")
-	if not storage.withdraw_stored_resource(Resources.Type.SILVER):
-		push_error("No silver in storage")
-	
-	storage.store_resources(Resources.Type.SILVER_BAR, 1)
--- projects/automation/scripts/buildings/furnance.gd
+++ projects/automation/scripts/buildings/furnance.gd
@@ -2,9 +2,35 @@ extends Node2D
 
 @onready var building_component: BuildingComponent = $BuildingComponent
 @onready var storage: StorageComponent = %Storage
+@onready var production_timer: Timer = %ProductionTimer
 
 
+func _ready():
+	production_timer.start()
 
 
 func _on_production_timer_timeout() -> void:
-	pass # Replace with function body.
+	if not storage.has_stored_resource(Resources.Type.COAL):
+		return
+	if storage.has_stored_resource(Resources.Type.COPPER):
+		_smelt_copper()
+	elif storage.has_stored_resource(Resources.Type.SILVER):
+		_smelt_silver()
+
+
+func _smelt_copper() -> void:
+	if not storage.withdraw_stored_resource(Resources.Type.COAL):
+		push_error("No Coal in storage")
+	if not storage.withdraw_stored_resource(Resources.Type.COPPER):
+		push_error("No copper in storage")
+	
+	storage.store_resources(Resources.Type.COPPER_BAR, 1)
+	
+
+func _smelt_silver() -> void:
+	if not storage.withdraw_stored_resource(Resources.Type.COAL):
+		push_error("No Coal in storage")
+	if not storage.withdraw_stored_resource(Resources.Type.SILVER):
+		push_error("No silver in storage")
+	
+	storage.store_resources(Resources.Type.SILVER_BAR, 1)
