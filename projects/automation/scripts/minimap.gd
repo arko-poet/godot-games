3b353718d87a057334f1072ddeb639946f29c0d6
arko <arkadiusz.kupracz@gmail.com>
2026-07-28 13:14:27 +0100

    feat(automation): add minimap

--- projects/automation/scripts/minimap.gd
+++ projects/automation/scripts/minimap.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
+@onready var camera: Camera2D = %Camera
+
 
 func _draw() -> void:
-	var camera_rect := Rect2(Vector2(25, 25), Vector2(50, 50))
+	var viewport_rect_size := get_viewport_rect().size
+	var camera_rect_size := _get_camera_rect_size()
+	var camera_rect := Rect2((viewport_rect_size - camera_rect_size) / 2.0, camera_rect_size)
 	draw_rect(camera_rect, Color.WHITE, false)
+
+
+func _get_camera_rect_size() -> Vector2:
+	if camera.zoom == Vector2(0.1, 0.1):
+		return get_viewport_rect().size
+	return get_viewport_rect().size
---
+++ projects/automation/scripts/test.gd
@@ -0,0 +1,6 @@
+extends SubViewport
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	world_2d = get_tree().root.world_2d
