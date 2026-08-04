508e8e3d1979fdd93ee089c28b0931378f74ff51
arko <arkadiusz.kupracz@gmail.com>
2026-08-04 18:56:10 +0100

    chore(automation): imrpove buiilding_component.gd

--- projects/automation/scripts/buildings/building_component.gd
+++ projects/automation/scripts/buildings/building_component.gd
@@ -1,22 +1,12 @@
 class_name Building
 extends Node2D
 
+const _DIRECTIONS := [Vector2i.RIGHT, Vector2i.DOWN, Vector2i.LEFT, Vector2i.UP]
+
 @export_range(0, INT32_MAX) var footprint_size: int = 0
 
 var center_cell: Vector2i
 
 
 func get_direction() -> Vector2i:
-	var rotation_count := int(round(rotation / (TAU / 4.0))) % 4
-	match rotation_count:
-		0:
-			return Vector2i.RIGHT
-		1:
-			return Vector2i.DOWN
-		2:
-			return Vector2i.LEFT
-		3:
-			return Vector2i.UP
-		_:
-			push_error("Invalid inserter direction")
-			return Vector2i.RIGHT
+	return _DIRECTIONS[int(round(rotation / (TAU / 4.0))) % 4]
