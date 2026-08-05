9b7159dcd9885cef865a68077f62a1b3b46e19c5
arko <arkadiusz.kupracz@gmail.com>
2026-08-05 14:24:12 +0100

    chore(automation): improve building_preview.gd

--- projects/automation/scripts/buildings/building_preview.gd
+++ projects/automation/scripts/buildings/building_preview.gd
@@ -1,20 +1,9 @@
 class_name BuildingPreview
 extends Node2D
 
-var footprint: Array[Vector2i]
-var center_tile: Vector2i
-
 var layer: TileMapLayer
 
-@onready var sprite: Sprite2D = $Sprite
-
-
-func _ready() -> void:
-	for i in 3:
-		for j in 3:
-			footprint.append(Vector2i(i, j))
-
-	center_tile = Vector2i(1, 1)
+@onready var sprite: Sprite2D = %Sprite
 
 
 func _process(_delta: float) -> void:
