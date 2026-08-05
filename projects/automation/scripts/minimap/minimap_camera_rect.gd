31f8382a58cc09882e69cff0dbbbb347d24852d8
arko <arkadiusz.kupracz@gmail.com>
2026-08-05 19:09:47 +0100

    chore(autiomation): improve minimap_camera_rect.gd

--- projects/automation/scripts/minimap/minimap_camera_rect.gd
+++ projects/automation/scripts/minimap/minimap_camera_rect.gd
@@ -1,14 +1,16 @@
 extends Control
 
 @onready var camera: Camera2D = %Camera
+@onready var minimap_camera: Camera2D = %MinimapCamera
 
 
 func _draw() -> void:
-	var viewport_rect_size := get_viewport_rect().size
-	var camera_rect_size := (viewport_rect_size / 5) / camera.zoom + Vector2.ONE
-	var camera_rect := Rect2((viewport_rect_size - camera_rect_size) / 2.0, camera_rect_size)
+	var main_viewport_size := camera.get_viewport_rect().size
+	var frustum_size := main_viewport_size / camera.zoom / World.TILE_SIZE
+	# + Vector2.ONE is so that rectangle is visible at all zoom levels
+	var camera_rect_size := frustum_size * minimap_camera.zoom + Vector2.ONE
+	var camera_rect := Rect2((get_viewport_rect().size - camera_rect_size) / 2.0, camera_rect_size)
 	draw_rect(camera_rect, Color.WHITE, false)
-	print(camera_rect)
 
 
 func _on_camera_zoom_changed(_new_zoom: Vector2) -> void:
