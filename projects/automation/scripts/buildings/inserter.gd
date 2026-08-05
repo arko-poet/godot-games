07f2bee60a9b0bb3bbde758befbc018534fdfbb5
arko <arkadiusz.kupracz@gmail.com>
2026-08-05 15:57:27 +0100

    chore(automation): improve inserter.gd code

--- projects/automation/scripts/buildings/inserter.gd
+++ projects/automation/scripts/buildings/inserter.gd
@@ -8,32 +8,54 @@ func _on_production_timer_timeout() -> void:
 	var direction := get_direction()
 
 	var source_node := world.get_node_at_cell(center_cell - direction)
-
-	var destination_node := world.get_node_at_cell(center_cell + direction)
-
-	if source_node == null:
-		return
-
-	var item: Item
-	if source_node is Belt:
-		if source_node.stored_item:
-			item = source_node.stored_item
-	else:
-		var storage: StorageComponent = source_node.get_node(^"Storage")
-		if not storage.has_stored_items():
-			return
-		item = storage.get_stored_item()
-		world.add_child(item)
-
+	var item := _take_item(source_node)
 	if not item:
 		return
 
-	if destination_node is Belt:
-		if destination_node.stored_item == null:
-			if source_node is Belt:
-				source_node.stored_item = null
-			destination_node.stored_item = item
-			item.position = destination_node.position
+	var target_node := world.get_node_at_cell(center_cell + direction)
+	if not _pass_item(item, target_node):
+		_refund_item(item, source_node)
+
+
+func _take_item(source_node: Node) -> Item:
+	if source_node == null:
+		return null
+
+	var item: Item
+	if source_node is Belt and source_node.stored_item:
+		item = source_node.stored_item
+		source_node.stored_item = null
 	else:
-		var storage: StorageComponent = destination_node.get_node(^"Storage")
-		storage.store_item(item)
+		var storage: StorageComponent = source_node.get_node(^"Storage")
+		if not storage or not storage.has_stored_items():
+			return null
+		item = storage.get_stored_item()
+
+	return item
+
+
+func _pass_item(item: Item, target_node: Node) -> bool:
+	if target_node == null:
+		return false
+
+	if target_node is Belt:
+		if target_node.stored_item == null:
+			target_node.stored_item = item
+			item.position = target_node.position
+			world.add_child(item)
+		else:
+			return false
+	else:
+		var storage: StorageComponent = target_node.get_node(^"Storage")
+		return storage and storage.store_item(item)
+
+	return true
+
+
+func _refund_item(item: Item, source_node: Node) -> void:
+	if source_node is Belt:
+		source_node.stored_item = item
+	else:
+		var storage: StorageComponent = source_node.get_node(^"Storage")
+		if storage:
+			storage.store_item(item)
